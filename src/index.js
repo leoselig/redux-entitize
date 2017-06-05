3d4cdac2bcc34c38de47837f7d410cb64df2b35a
Leo Selig <dev.selig@gmail.com>
2017-06-05 19:55:10 +0200

    Fix overriding default exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 // @flow
 
-export * from "./reducer";
-export * from "./actions";
+import createEntitiesReducer, { type StateType } from "./reducer";
+import { updateEntitiesAction, type UpdateEntitiesActionType } from "./actions";
+
+export { createEntitiesReducer, updateEntitiesAction };
+export type { StateType, UpdateEntitiesActionType };
