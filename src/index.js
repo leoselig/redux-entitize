f620d8f07a532f01a6befe50e7dc8c69b61e9904
Leo Selig <dev.selig@gmail.com>
2017-06-30 09:41:56 +0200

    Add first selectors (`selectEntity` and `selectEntities`)
    - skipped an implementation based on `reselect` for now (this might change
      when we gain experience how typical memoized selectors can look like)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@
 
 import createEntitiesReducer, { type StateType } from "./reducer";
 import { updateEntitiesAction, type UpdateEntitiesActionType } from "./actions";
+import { selectEntity, selectEntities } from "./selectors";
 
-export { createEntitiesReducer, updateEntitiesAction };
+export {
+  createEntitiesReducer,
+  updateEntitiesAction,
+  selectEntity,
+  selectEntities
+};
 export type { StateType, UpdateEntitiesActionType };
