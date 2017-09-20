c6d4ad3d0183708b9379414aa8852c228f270559
Leo Selig <dev.selig@gmail.com>
2017-09-20 15:41:58 +0200

    Reorganize reducer tests
    Suite hierarchy is now: tested action type -> initilized schema structure
    -> action payload.

--- test/specs/reducer.spec.js
+++ test/specs/reducer.spec.js
@@ -11,132 +11,150 @@ import {
 import { createSpyStore } from "../utils";
 
 describe("reducer", () => {
-  describe("when initilized with a single-entity schema", () => {
-    function setupSingleEntityStore(
-      state = {
-        articles: {}
-      }
-    ) {
-      return createSpyStore(
-        { entities: state },
-        {
-          entities: createEntitiesReducer({
-            articles: new schema.Entity("articles")
-          })
-        }
-      );
+  function setupSingleEntityStore(
+    state = {
+      articles: {}
     }
-    describe("when receiving DELETE_ENTITY action", () => {
-      describe("with known entity", () => {
-        test("removes entity from state", () => {
-          const store = setupSingleEntityStore({
-            articles: {
-              article_1: {}
-            }
-          });
+  ) {
+    return createSpyStore(
+      { entities: state },
+      {
+        entities: createEntitiesReducer({
+          articles: new schema.Entity("articles")
+        })
+      }
+    );
+  }
 
-          expect(Object.keys(store.getState().entities.articles)).toContain(
-            "article_1"
-          );
+  function setupNestedEntityStore(
+    state = {
+      articles: {},
+      authors: {}
+    }
+  ) {
+    return createSpyStore(
+      { entities: state },
+      {
+        entities: createEntitiesReducer({
+          articles: new schema.Entity("articles", {
+            author: new schema.Entity("authors")
+          })
+        })
+      }
+    );
+  }
 
-          store.dispatch(deleteEntityAction("articles", "article_1"));
+  function setupArrayEntityStore(
+    state = {
+      articles: {},
+      comments: {}
+    }
+  ) {
+    return createSpyStore(
+      { entities: state },
+      {
+        entities: createEntitiesReducer({
+          articles: new schema.Entity("articles", {
+            comments: [new schema.Entity("comments")]
+          })
+        })
+      }
+    );
+  }
 
-          expect(Object.keys(store.getState().entities.articles)).not.toContain(
-            "article_1"
-          );
+  describe("when receiving DELETE_ENTITY action", () => {
+    describe("with known entity", () => {
+      test("removes entity from state", () => {
+        const store = setupSingleEntityStore({
+          articles: {
+            article_1: {}
+          }
         });
+
+        expect(Object.keys(store.getState().entities.articles)).toContain(
+          "article_1"
+        );
+
+        store.dispatch(deleteEntityAction("articles", "article_1"));
+
+        expect(Object.keys(store.getState().entities.articles)).not.toContain(
+          "article_1"
+        );
       });
     });
-    describe("when receiving UPDATE_ENTITY action", () => {
-      describe("with new entity", () => {
-        test("puts entity into state", () => {
-          const store = setupSingleEntityStore();
+  });
+  describe("when receiving UPDATE_ENTITY action", () => {
+    describe("with new entity", () => {
+      test("puts entity into state", () => {
+        const store = setupSingleEntityStore();
 
-          store.dispatch(
-            updateEntityAction("articles", {
-              id: "article_1",
-              title: "Foo Bar"
-            })
-          );
+        store.dispatch(
+          updateEntityAction("articles", {
+            id: "article_1",
+            title: "Foo Bar"
+          })
+        );
 
-          expect(store.getState()).toEqual({
-            entities: {
-              articles: {
-                article_1: {
-                  id: "article_1",
-                  title: "Foo Bar"
-                }
-              }
-            }
-          });
-        });
-      });
-      describe("with known entity", () => {
-        test("merges old and new entity", () => {
-          const store = setupSingleEntityStore({
+        expect(store.getState()).toEqual({
+          entities: {
             articles: {
               article_1: {
-                foo: "stays the same",
-                bar: "will change"
+                id: "article_1",
+                title: "Foo Bar"
               }
             }
-          });
-
-          store.dispatch(
-            updateEntityAction("articles", {
-              id: "article_1",
-              bar: "changed",
-              baz: "is new"
-            })
-          );
-
-          expect(store.getState()).toEqual({
-            entities: {
-              articles: {
-                article_1: {
-                  id: "article_1",
-                  foo: "stays the same",
-                  bar: "changed",
-                  baz: "is new"
-                }
-              }
-            }
-          });
-        });
-      });
-
-      describe("without id", () => {
-        test("throws an error", () => {
-          const store = setupSingleEntityStore();
-
-          expect(() => {
-            store.dispatch(
-              updateEntityAction("articles", {
-                title: "ID missing"
-              })
-            );
-          }).toThrow("No 'id'-field found in entitiy of schema 'articles'");
+          }
         });
       });
     });
-    describe("when initilized with a nested entity schema", () => {
-      function setupNestedEntityStore(
-        state = {
-          articles: {},
-          authors: {}
-        }
-      ) {
-        return createSpyStore(
-          { entities: state },
-          {
-            entities: createEntitiesReducer({
-              articles: new schema.Entity("articles", {
-                author: new schema.Entity("authors")
-              })
-            })
+    describe("with known entity", () => {
+      test("merges old and new entity", () => {
+        const store = setupSingleEntityStore({
+          articles: {
+            article_1: {
+              foo: "stays the same",
+              bar: "will change"
+            }
           }
+        });
+
+        store.dispatch(
+          updateEntityAction("articles", {
+            id: "article_1",
+            bar: "changed",
+            baz: "is new"
+          })
         );
-      }
+
+        expect(store.getState()).toEqual({
+          entities: {
+            articles: {
+              article_1: {
+                id: "article_1",
+                foo: "stays the same",
+                bar: "changed",
+                baz: "is new"
+              }
+            }
+          }
+        });
+      });
+    });
+    describe("without id", () => {
+      test("throws an error", () => {
+        const store = setupSingleEntityStore();
+
+        expect(() => {
+          store.dispatch(
+            updateEntityAction("articles", {
+              title: "ID missing"
+            })
+          );
+        }).toThrow("No 'id'-field found in entitiy of schema 'articles'");
+      });
+    });
+
+    describe("when initilized with a nested entity schema", () => {
       describe("with new entity", () => {
         test("puts entity and nested entity into each state", () => {
           const store = setupNestedEntityStore();
@@ -226,111 +244,58 @@ describe("reducer", () => {
           });
         });
       });
-    });
-    describe("when initilized with an array entity schema", () => {
-      function setupArrayEntityStore(
-        state = {
-          articles: {},
-          comments: {}
-        }
-      ) {
-        return createSpyStore(
-          { entities: state },
-          {
-            entities: createEntitiesReducer({
-              articles: new schema.Entity("articles", {
-                comments: [new schema.Entity("comments")]
+      describe("when initilized with an array entity schema", () => {
+        describe("with new entity", () => {
+          test("puts entity and nested entity into each state", () => {
+            const store = setupArrayEntityStore();
+
+            store.dispatch(
+              updateEntityAction("articles", {
+                id: "article_1",
+                title: "Foo Bar",
+                comments: [
+                  {
+                    id: "comment_1",
+                    title: "Good"
+                  },
+                  {
+                    id: "comment_2",
+                    title: "Bad"
+                  }
+                ]
               })
-            })
-          }
-        );
-      }
-      describe("with new entity", () => {
-        test("puts entity and nested entity into each state", () => {
-          const store = setupArrayEntityStore();
+            );
 
-          store.dispatch(
-            updateEntityAction("articles", {
-              id: "article_1",
-              title: "Foo Bar",
-              comments: [
-                {
-                  id: "comment_1",
-                  title: "Good"
+            expect(store.getState()).toEqual({
+              entities: {
+                articles: {
+                  article_1: {
+                    id: "article_1",
+                    title: "Foo Bar",
+                    comments: ["comment_1", "comment_2"]
+                  }
                 },
-                {
-                  id: "comment_2",
-                  title: "Bad"
-                }
-              ]
-            })
-          );
-
-          expect(store.getState()).toEqual({
-            entities: {
-              articles: {
-                article_1: {
-                  id: "article_1",
-                  title: "Foo Bar",
-                  comments: ["comment_1", "comment_2"]
-                }
-              },
-              comments: {
-                comment_1: {
-                  id: "comment_1",
-                  title: "Good"
-                },
-                comment_2: {
-                  id: "comment_2",
-                  title: "Bad"
+                comments: {
+                  comment_1: {
+                    id: "comment_1",
+                    title: "Good"
+                  },
+                  comment_2: {
+                    id: "comment_2",
+                    title: "Bad"
+                  }
                 }
               }
-            }
+            });
           });
         });
-      });
-      describe("with known entity", () => {
-        test("merges old and new entity", () => {
-          const store = setupArrayEntityStore({
-            articles: {
-              article_1: {
-                foo: "stays the same",
-                bar: "will change",
-                comments: ["comment_1"]
-              }
-            },
-            comments: {
-              comment_1: {
-                id: "comment_1",
-                foo: "stays the same",
-                bar: "will change"
-              }
-            }
-          });
-
-          store.dispatch(
-            updateEntityAction("articles", {
-              id: "article_1",
-              bar: "changed",
-              baz: "is new",
-              comments: [
-                {
-                  id: "comment_1",
-                  bar: "changed",
-                  baz: "is new"
-                }
-              ]
-            })
-          );
-
-          expect(store.getState()).toEqual({
-            entities: {
+        describe("with known entity", () => {
+          test("merges old and new entity", () => {
+            const store = setupArrayEntityStore({
               articles: {
                 article_1: {
-                  id: "article_1",
                   foo: "stays the same",
-                  bar: "changed",
-                  baz: "is new",
+                  bar: "will change",
                   comments: ["comment_1"]
                 }
               },
@@ -338,47 +303,83 @@ describe("reducer", () => {
                 comment_1: {
                   id: "comment_1",
                   foo: "stays the same",
-                  bar: "changed",
-                  baz: "is new"
+                  bar: "will change"
                 }
               }
-            }
+            });
+
+            store.dispatch(
+              updateEntityAction("articles", {
+                id: "article_1",
+                bar: "changed",
+                baz: "is new",
+                comments: [
+                  {
+                    id: "comment_1",
+                    bar: "changed",
+                    baz: "is new"
+                  }
+                ]
+              })
+            );
+
+            expect(store.getState()).toEqual({
+              entities: {
+                articles: {
+                  article_1: {
+                    id: "article_1",
+                    foo: "stays the same",
+                    bar: "changed",
+                    baz: "is new",
+                    comments: ["comment_1"]
+                  }
+                },
+                comments: {
+                  comment_1: {
+                    id: "comment_1",
+                    foo: "stays the same",
+                    bar: "changed",
+                    baz: "is new"
+                  }
+                }
+              }
+            });
           });
         });
       });
     });
-    describe("when receiving UPDATE_ENTITIES action", () => {
-      describe("with new entities", () => {
-        test("puts entities into state", () => {
-          const store = setupSingleEntityStore();
+  });
+  describe("when receiving UPDATE_ENTITIES action", () => {
+    describe("with new entities", () => {
+      test("puts entities into state", () => {
+        const store = setupSingleEntityStore();
 
-          store.dispatch(
-            updateEntitiesAction("articles", [
-              {
+        store.dispatch(
+          updateEntitiesAction("articles", [
+            {
+              id: "article_1",
+              title: "Foo Bar 1"
+            },
+            {
+              id: "article_2",
+              title: "Foo Bar 2"
+            }
+          ])
+        );
+
+        expect(store.getState()).toEqual({
+          entities: {
+            articles: {
+              article_1: {
                 id: "article_1",
                 title: "Foo Bar 1"
               },
-              {
+              article_2: {
                 id: "article_2",
                 title: "Foo Bar 2"
               }
-            ])
-          );
-
-          expect(store.getState()).toEqual({
-            entities: {
-              articles: {
-                article_1: {
-                  id: "article_1",
-                  title: "Foo Bar 1"
-                },
-                article_2: {
-                  id: "article_2",
-                  title: "Foo Bar 2"
-                }
-              }
             }
-          });
+          }
         });
       });
     });
